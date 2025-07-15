f4eef0c5d4be177a126b245c1b631c43488b2d02
SashaV228 <sasha.vpan@gmail.com>
2025-07-15 13:09:53 +1000

    Сделал изначальный вариант но не работает put. Нажо поменть вариант от дипски на мре выполнение прямл как в пост

--- HttpClientForSQL/MainWindow.xaml.cs
+++ HttpClientForSQL/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using HttpClientForSQL;
 using AspForSQL;
+using Newtonsoft.Json;
 
 namespace HttpClientForSQL
 {
@@ -40,7 +41,63 @@ namespace HttpClientForSQL
             APIServer aPIServer = new APIServer();
             await CombineToLibrary();
             int answer = await aPIServer.PostLibraryAsync(localLibrary);
-            Id_label.Content = answer;
+            StatusCode_Label.Content = answer;
+            await ChangeStatusColor(answer);
+        }
+
+        private async Task ChangeStatusColor(int content)
+        {
+            string str = content.ToString();
+            char first = str[0];
+            switch (first) 
+            {
+                case '2':
+                    StatusCode_Label.Foreground = Brushes.Green;
+                        break;
+                case '4':
+                    StatusCode_Label.Foreground = Brushes.Red;
+                    break;
+                default:
+                    StatusCode_Label.Foreground= Brushes.Purple;
+                    break;
+            }
+                
+        }
+
+        private async void Get_Button_Click(object sender, RoutedEventArgs e)
+        {
+            APIServer api = new APIServer();
+            int id = Int32.Parse(Id_TB.Text);
+            var answer = await api.GetLibraryAsync(id);
+            GetResult_TB.Text = $"{answer.Author}, {answer.Name}, {answer.Year}";
+
+        }
+
+        private async void Put_Button_Click(object sender, RoutedEventArgs e)
+        {
+            await Task.Run(() =>
+            { 
+                APIServer api = new APIServer();
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    Library lib = new Library
+                    {
+                        Author = AuthorForPut_TB.Text,
+                        Name = NameForPut_TB.Text,
+                        Year = Int32.Parse(YearForPut_TB.Text)
+                    };
+
+
+                    var id = Int32.Parse(IdForPut_TB.Text);
+
+                    var answer = api.UpdateLibraryAsync(id, lib);
+                    StatusCode_Label.Content = answer;
+                    Console.WriteLine(answer);
+                });
+
+                //ChangeStatusColor(answer);
+            });
+
         }
     }
 }
